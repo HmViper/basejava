8ba450edebaa9a6e639b1da6e726edda8ad0e3d9
Cyrill <mazurink@mail.ru>
2020-10-03 15:19:08 +0300

    correct "get" method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -28,9 +28,6 @@ public class ArrayStorage {
                     return storage[i];
                 }
             }
-            else {
-                return null;
-            }
         }
         return null;
     }
