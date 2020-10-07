3a99e36043ea8c075fc0d6206134e59ea3d6ad39
Cyrill <mazurink@mail.ru>
2020-10-07 22:17:07 +0300

    HW#2 completed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,6 +5,15 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int size;
 
+    int searchResume(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
@@ -12,28 +21,40 @@ public class ArrayStorage {
         size = 0;
     }
 
+    void update(Resume r) {
+        int index;
+        if ((index = searchResume(r.uuid)) > -1) {
+            storage[index] = r;
+        } else {
+            System.out.println("ERROR: Resume is not present");
+        }
+    }
+
     void save(Resume r) {
-        storage[size++] = r;
+        if (searchResume(r.uuid) > -1) {
+            System.out.println("ERROR: Resume is already saved");
+        } else if (size == storage.length) {
+            System.out.println("ERROR: Storage overload");
+        } else {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                return storage[i];
-            }
-        }
-        return null;
+        return searchResume(uuid) > -1 ? storage[searchResume(uuid)] : null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                storage[i] = storage[size-1];
-                storage[size-1] = null;
-                size--;
-            }
+        int index;
+        if ((index = searchResume(uuid)) > -1) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("ERROR: Resume in not present");
         }
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
